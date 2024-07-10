d313fcb998ef1dcda5bb58adf6b76951d058a3ac
Huy <tuanhuyanime@gmail.com>
2024-07-10 15:47:38 +0700

    bai1

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center"></h2>
+        <img src="https://monspet.com/wp-content/uploads/2019/07/uploads2Fcard2Fimage2F4581502F31d70745-4fbb-4630-a27d-cc0591c780b7.jpg2F950x534__filters3Aquality288029-768x432.jpg" alt="Welcome Image" className="welcome-image" />
+      </div>
+    );
+  }
+}
+
+export default Home;
